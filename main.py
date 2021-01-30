231c9fe08456a518772e71571b7ee4ed189e8263
YumNumm <73390859+YumNumm@users.noreply.github.com>
2021-01-30 21:54:14 +0900

    Update main.py
    + 複数人の体温送信に対応
    - TwitterやLINE APIは未実装

--- main.py
+++ main.py
@@ -1,65 +1,81 @@
-import json
-import os
-import random
+#!/usr/bin/env python3
+
+# IDとパスワードの設定
+
+# YSFH-SINE ID/Password
+UserID = []
+UserPW = []
+
+
+# Twitter API Keys
+CK = ""
+CS = ""
+AT = ""
+AS = ""
+
+# LINE Notify API Token
+LT = ""
+
+# 体温の乱数生成の幅設定
+TempMin = 36.0
+TempMax = 36.8
+
+
+# ライブラリをインポート
+import subprocess
 import sys
-import time
-import traceback
-from decimal import Decimal
 
-import requests
-import tweepy
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
+try:
 
-for i in range(len(sys.argv)):
-    print(sys.argv[i])
+    import json
+    import os
+    import random
+    import time
+    import traceback
+    from decimal import Decimal
 
+    import requests
+    import tweepy
+    from selenium import webdriver
+    from selenium.webdriver.common.action_chains import ActionChains
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.support import expected_conditions
+    from selenium.webdriver.support.wait import WebDriverWait
+    from webdriver_manager.chrome import ChromeDriverManager
 
-def check():
-    # ユーザ名とパスワードの確認
+except:
+    print("importに失敗 インストールします")
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "-r", "requirements.txt"]
+    )
     try:
-        user_name = str(sys.argv[1])
-        user_password = str(sts.argv[2])
+        import json
+        import os
+        import random
+        import time
+        import traceback
+        from decimal import Decimal
+
+        import requests
+        import tweepy
+        from selenium import webdriver
+        from selenium.webdriver.common.action_chains import ActionChains
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+        from selenium.webdriver.common.keys import Keys
+        from selenium.webdriver.support import expected_conditions
+        from selenium.webdriver.support.wait import WebDriverWait
+        from webdriver_manager.chrome import ChromeDriverManager
+
     except:
-        print("[ Error ] IDとパスワードが確認できませんでした。もう一度入力してください")
-        print("IDを入力してください")
-        user_name = input()
-        print("パスワードを入力してください")
-        user_password = str(input())
-
-    print("ID:" + user_name + "\nパスワード:" + user_password + "\nを利用します")
-    photo_name = user_name + ".png"
-    # 体温設定
-    try:
-        temp = str(sys.argv[3])
-        print("[ Info ] 体温は" + temp + "を使用します(引数より)")
-    except:
-        temp = str(random.uniform(36.0, 36.7))
-        print("[ Info ] 体温は" + temp + "を使用します(乱数生成)")
-    return 0
+        print("\n\nインストール失敗。pipから自分でインストールしてください。")
+        exit(0)
+    print("\n\nインストール完了\n\n")
 
 
-def twisend():
-    CK = "XXX"
-    CS = "XXX"
-    AT = "XXX"
-    AS = "XXX"
-
-    # Twitterオブジェクトの生成
-    auth = tweepy.OAuthHandler(CK, CS)
-    auth.set_access_token(AT, AS)
-    api = tweepy.API(auth)
-    return 0
-
-
-def chromesend():
-    t1 = time.time()
+def chromesend(ID, PW, TEMP):
     try:
         print("Starting Chrome...")
         chrome_option = webdriver.ChromeOptions()
@@ -75,8 +91,8 @@ def chromesend():
         driver.get("https://www.ysfh.ed.jp/auth/login")
         driver.set_window_size(512, 512)
         driver.find_element(By.ID, "UserUsername").click()
-        driver.find_element(By.ID, "UserUsername").send_keys(user_name)
-        driver.find_element(By.ID, "UserPassword").send_keys(user_password)
+        driver.find_element(By.ID, "UserUsername").send_keys(ID)
+        driver.find_element(By.ID, "UserPassword").send_keys(PW)
         driver.find_element(By.ID, "UserPassword").send_keys(Keys.ENTER)
         driver.find_element(
             By.CSS_SELECTOR, "#MenuFramesPageMajor21072 > .pull-left"
@@ -94,7 +110,7 @@ def chromesend():
         ).send_keys(Keys.DELETE)
         driver.find_element(
             By.ID, "RegistrationAnswerBbceb1b7d337c58f2e972fbc908f97310AnswerValue"
-        ).send_keys(temp)
+        ).send_keys(TEMP)
         driver.find_element(
             By.ID,
             "RegistrationAnswer44b3e23a28d39ea0840898d7b7bc8c590AnswerValueC3056ac26d8d0590690d4c81e11dc1f8登校予定",
@@ -109,7 +125,7 @@ def chromesend():
         driver.save_screenshot("screenshot.png")
         driver.find_element(By.LINK_TEXT, "終了").click()
 
-        driver.save_screenshot(photo_name)
+        driver.save_screenshot(PhotoName)
         driver.quit()
         print("Posting finished.")
     except:
@@ -117,30 +133,79 @@ def chromesend():
         driver.quit()
 
 
-def fin():
-    t2 = time.time()
-    elst = t2 - t1
-    print("経過時間は", elst)
+try:
+    # sys.argvの個数を確認
+    count = len(sys.argv)
+    print(count)
 
-    text = "体温送信が完了しました\n実行時間は:" + elst + "秒"
+    if count == 4:
+        TempNow = float(sys.argv[1])
+        UserID = str(sys.argv[2])
+        UserPW = str(sys.argv[3])
+        TempSet = 0
+        SetUser = 0
 
-    api.update_with_media(filename="./screenshot.png", status=text)
+    elif count == 3:
+        UserID = str(sys.argv[1])
+        UserPW = str(sys.argv[2])
+        TempSet = 1
+        SetUser = 0
 
-    token = "XXX"
-    url = "https://notify-api.line.me/api/notify"
-    headers = {"Authorization": "Bearer " + token}
-    payload = {"message": text}
-    files = {"imageFile": open("./screenshot.png", "rb")}
-    line_notify = requests.post(url, data=payload, headers=headers, files=files)
+    elif count == 2:
+        TempNow = float(sys.argv[1])
+        TempSet = 0
+        SetUser = 1
 
-    print("終了")
+    elif count == 1:
+        TempSet = 1
+        SetUser = 1
+
+    else:
+        raise ValueError("Error")
+
+except:
+    print("引数の順番が正しいか再確認してください。")
+    sys.exit()
+
+if len(UserID) != 0:
+    SetUser = 0
 
 
-def main():
-    check()
-    twisend()
-    chromesend()
+if TempSet == 1:
+    TempNow = str(
+        Decimal(str(random.uniform(TempMin, TempMax))).quantize(Decimal("0.1"))
+    )
+if SetUser == 1:
+    UserID = str(input("IDを入力してください : "))
+    UserPW = str(input("パスワードを入力してください : "))
+
+# Twitter API KEYが定義されているかを確認
+if CK == CS == AT == AS:
+    TwiSend = 0
+else:
+    TwiSend = 1
+
+# LINE API Keyが定義されているかを確認
+if LT == "":
+    LineSend = 0
+else:
+    LineSend = 1
+
+if len(UserID) == len(UserPW):
+    print()
+else:
+    print("事前設定したIDとパスワードの個数が正しくありません。\n再確認してください。")
+    sys.exit(1)
 
 
-if __name__ == "__main__":
-    main()
+UserID = UserID.split(",")
+UserPW = UserPW.split(",")
+
+
+# 繰り返し回数判定
+for i in range(len(UserID)):
+    ID = UserID[i]
+    PW = UserPW[i]
+    PhotoName = str(UserID[i]) + ".png"
+    t1 = time.time()
+    chromesend(ID, PW, TempNow)
